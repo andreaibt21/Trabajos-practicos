6977a329a84627046b5e8341a009f57d6e558a91
andreaibt21 <andreaixabella@gmail.com>
2021-04-11 23:34:31 -0300

    Menú creado, suma empezada

--- TP1/src/TP1.c
+++ TP1/src/TP1.c
@@ -1,17 +1,68 @@
 /*
  ============================================================================
  Name        : TP1.c
- Author      : 
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
+ Author      : 	ANDREA BRICEÑO
  ============================================================================
+
+1. Ingresar 1er operando (A=x)
+2. Ingresar 2do operando (B=y)
+3. Calcular todas las operaciones
+a) Calcular la suma (A+B)
+b) Calcular la resta (A-B)
+c) Calcular la division (A/B)
+d) Calcular la multiplicacion (A*B)
+e) Calcular el factorial (A!)
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "calculos.h"
+
+
 int main(void) {
-	puts("ACÁ VA EL TP1!!"); /* prints !!!Hello World!!! */
-	return EXIT_SUCCESS;
-}
+	setbuf(stdout,NULL);
+
+	utn_menu();
+
+		return EXIT_SUCCESS;
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+//a) Calcular la suma (A+B)
+
+
+ * breifCalcula la suma de dos numeros
+ * paraam1 recibe un numero entero para sumar
+ * paraam2 recibe un numero entero para sumar
+ * Retorno devuelve la suma de los numeros recibidos por parametros
+ *
+
+
+
+int utn_getSuma(int A, int B);
+
+ int utn_getSuma((int A, int B)
+{
+	int A;
+	int B;
+	int resultadoSuma;
+
+	resultadoSuma = A+B;
+
+
+	return resultadoSuma;
+};
+*/
